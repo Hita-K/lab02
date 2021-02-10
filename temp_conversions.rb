c575a4c29036acb33a5cc0cc7e4179c93baa8430
Hita-K <68398983+Hita-K@users.noreply.github.com>
2021-02-09 20:33:46 -0500

    Add files via upload
    Added both C -> F and F -> C

--- temp_conversions.rb
+++ temp_conversions.rb
@@ -1,13 +1,24 @@
-def convert(temp)
+def convert(temp, measure="F")
 	return "Temperature must be an integer" unless temp.class == Integer
 	return "Temperature below Absolute Zero" if temp < -474 
-	5 * (temp - 32) / 9
+	if measure == "F"
+		5 * (temp - 32) / 9
+	else
+		9 * (temp + 32) / 5
+
+	end
 
 end
 
 puts convert(32)
 puts convert(50)
 puts convert(212)
+
+puts convert(0, "C")
+puts convert(10, "C")
+puts convert(100, "C")
+puts convert(-280, "C")
+
+
 puts convert("zero")
 puts convert(-500)
-
