3283bcfc0d8e8749eb63906369010312a11080be
Hita-K <68398983+Hita-K@users.noreply.github.com>
2021-02-09 20:19:58 -0500

    Add files via upload
    Added Absolute Zero exception

--- temp_conversions.rb
+++ temp_conversions.rb
@@ -1,5 +1,6 @@
 def convert(temp)
 	return "Temperature must be an integer" unless temp.class == Integer
+	return "Temperature below Absolute Zero" if temp < -474 
 	5 * (temp - 32) / 9
 
 end
@@ -8,4 +9,5 @@ puts convert(32)
 puts convert(50)
 puts convert(212)
 puts convert("zero")
+puts convert(-500)
 
